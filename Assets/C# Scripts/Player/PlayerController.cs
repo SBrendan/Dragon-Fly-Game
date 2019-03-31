cf601e1a374e1c1cbc244d6c3762ab92276e60c6
SBrendan <brendan.seiller2016@campus-eni.fr>
2019-03-31 21:05:47 +0200

    Add dragon movement

---
+++ Assets/C# Scripts/Player/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Vector2 targetPos;
+    public float Yincrement;
+    public float Xincrement;
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y + Yincrement);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            targetPos = new Vector2(transform.position.x + Xincrement, transform.position.y);
+            transform.position = targetPos;
+        }
+        else if (Input.GetKeyDown(KeyCode.RightArrow) && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y - Yincrement);
+            transform.position = targetPos;
+        }
+    }
+}
