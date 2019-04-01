c9414a51a4473f9f6718a64b8f5150a2e6815e23
Yoo33 <horusalwayswin@hotmail.com>
2019-04-01 22:48:45 +0200

    This shit now shoot!

--- Assets/C# Scripts/Player/PlayerController.cs
+++ Assets/C# Scripts/Player/PlayerController.cs
@@ -5,6 +5,11 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
+    public Animator animator;
+    public float FireDelay = 0.6f;
+    float FireTimer;
+    public GameObject Fireball;
+    public Vector2 FireballOffset = new Vector2(1.3f, -0.3f);
     private Vector2 targetPos;
     public float Yincrement;
     public float Xincrement;
@@ -35,6 +40,19 @@ public class PlayerController : MonoBehaviour
         {
             targetPos = new Vector2(transform.position.x - Xincrement, transform.position.y - Yincrement);
             transform.position = targetPos;
+        }
+
+        FireTimer -= Time.deltaTime;
+        if (Input.GetKey(KeyCode.Space) && FireTimer <= 0f)
+        {
+            animator.SetBool("IsShooting", true);
+            Debug.Log("BIM!");
+            Instantiate(Fireball, new Vector2(transform.position.x + FireballOffset.x, transform.position.y + FireballOffset.y), Quaternion.identity);
+            FireTimer = FireDelay;
+        }
+        else
+        {
+            animator.SetBool("IsShooting", false);
         }
     }
 }
