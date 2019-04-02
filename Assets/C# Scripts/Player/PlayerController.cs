b6380d1269a8852dd33338e240e42beb2a389fd0
JayLvis <seillerbrendan@gmail.com>
2019-04-02 22:08:50 +0200

    Revert "Added some collisions on fireball, main character... AND A F***ING BOX"

--- Assets/C# Scripts/Player/PlayerController.cs
+++ Assets/C# Scripts/Player/PlayerController.cs
@@ -5,22 +5,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
 
-    // Shooting stuff
     public Animator animator;
     public float FireDelay = 0.6f;
     float FireTimer;
     public GameObject Fireball;
     public Vector2 FireballOffset = new Vector2(1.3f, -0.3f);
-
-    // Moving stuff
     private Vector2 targetPos;
     public float Yincrement;
     public float Xincrement;
     private GameMenuController gameMenuController;
     private bool gameIsPaused;
-
-    // Misc
-    public float health = 3f;
 
     private void Start()
     {
@@ -98,11 +92,6 @@ public class PlayerController : MonoBehaviour
         else
         {
             animator.SetBool("IsShooting", false);
-        }
-        if (health <= 0f)
-        {
-            Debug.Log("PAN T MOR");
-            Destroy(gameObject);
         }
     }
 
