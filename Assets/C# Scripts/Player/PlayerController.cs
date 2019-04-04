22916027f40a1eccd1abc3012a0b00e7a159ddf7
Yoo33 <horusalwayswin@hotmail.com>
2019-04-04 23:12:03 +0200

    Added box collisions, THAT SHIT KILLS YOU BRO
    Deleted level1
    
    Delete Level1.unity

--- Assets/C# Scripts/Player/PlayerController.cs
+++ Assets/C# Scripts/Player/PlayerController.cs
@@ -53,4 +53,13 @@ public class PlayerController : MonoBehaviour
             Destroy(gameObject);
         }
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ennemies"))
+        {
+            health -= collision.GetComponent<Ennemies>().damage;
+            animator.SetTrigger("TakeDamage");
+        }
+    }
 }
