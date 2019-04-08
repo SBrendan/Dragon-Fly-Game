a6d851d94deb93abd3aa734ee9c1c609f474d3ec
Yoo33 <horusalwayswin@hotmail.com>
2019-04-08 22:07:41 +0200

    Add scorer

---
+++ Assets/C# Scripts/Player/Scorer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+
+
+public class Scorer : MonoBehaviour
+{
+    public float Score = 0f;
+    public Text ScoreDisplay;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScoreDisplay.text = Score.ToString();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Ennemies"))
+        {
+            Score += collision.GetComponent<Ennemies>().avoid_point;
+            Debug.Log("Score : " + Score.ToString());
+        }
+    }
+}
